2fef7de2a2ee3d07ba08c6b2dfacb2d94d7a55cb
chotu222 <ckrackerx15@gmail.com>
2013-03-03 10:53:46 +0530

    Doc and include

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Thu Jan 3 18:14:26 EST 2013"
+#define LINUX_COMPILE_BY "koo"
+#define LINUX_COMPILE_HOST "koo-desktop"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
